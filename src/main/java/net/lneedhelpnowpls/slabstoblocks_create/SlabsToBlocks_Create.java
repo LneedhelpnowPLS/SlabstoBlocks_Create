0f0923d4a20a85d8ecc9e3217f94ce6f8ab47dfe
LneedhelpnowPLS <marcushadriantextor@gmail.com>
2025-02-27 19:04:49 -0500

    Comment removal to compact lines down

--- src/main/java/net/lneedhelpnowpls/slabstoblocks_create/SlabsToBlocks_Create.java
+++ src/main/java/net/lneedhelpnowpls/slabstoblocks_create/SlabsToBlocks_Create.java
@@ -11,7 +11,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(SlabsToBlocks_Create.MOD_ID)
 public class SlabsToBlocks_Create {
     public static final String MOD_ID = "slabstoblocks_create";
@@ -29,7 +28,6 @@ public class SlabsToBlocks_Create {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
